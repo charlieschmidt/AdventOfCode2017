a8d07d66dfd7eff211eaf0bf72ff061c6e4d40ea
Charlie Schmidt <charlie@charlieschmidt.me>
2017-12-05 21:51:56 -0500

    get rid of temporary variables add comments

--- day4.ps1
+++ day4.ps1
@@ -11,9 +11,17 @@ begin {
 
 process {
     if ($part -eq 1) {
-        $in | ? {$words = $_ -split " "; $words.Count -eq ($words | select -Unique).Count} | % { $valid++ }
+        , ($in -split " ") | ? { # convert the space separated string into a pipeable array, pipe to where
+            $_.Count -eq ($_ | select -Unique).Count # array elements compared to unique array elements
+        } | % { # pipe to foreach, will only be 0 or 1 pipeline iterations here
+            $valid++
+        }
     } else {
-        $in | ? {$words = $_ -split " " | % { ($_.tochararray() | sort) -join '' } ; $words.Count -eq ($words | select -Unique).Count} | % { $valid++}        
+        , ($in -split " ") | ? {  # convert the space separated string into a pipeable array
+            $_.Count -eq ($_ | % {($_.tochararray() | sort) -join '' } | select -unique).Count # array elements compared to unique sorted-characted array elements
+        } | % {  # pipe to foreach, will only be 0 or 1 pipeline iterations here
+            $valid++ 
+        }
     }
 }
 
