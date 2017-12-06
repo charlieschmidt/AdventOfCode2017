9112c990b6944a374b21537007688692e9fe5aae
Charlie Schmidt <charlie@charlieschmidt.me>
2017-12-05 21:50:58 -0500

    add comments for day2

--- day2.ps1
+++ day2.ps1
@@ -11,9 +11,22 @@ begin {
 process 
 {
     if ($part -eq 1) {
+        <#
+            split the input on tabs, pipe to measure
+            measure the min and max values, pipe the measureresult to select
+            select a new property that is the difference
+            increment total by the different value
+        #>
         $total += ($in -split '\t' | measure -min -max | select @{n = "d"; e = {$_.maximum - $_.minimum}}).d
     } else {
-        $total += $in -split '\t' | % { $x = $_; $in -split '\t' |? {$_ -ne $x -and $x % $_ -eq 0} | % {$x / $_ } }
+        $total += $in -split '\t' | % { # split the input on tabs, pipe to foreach (a)
+            $x = $_
+            $in -split '\t' |? { # resplit the input, pipe to where (b)
+                $_ -ne $x -and $x % $_ -eq 0 # where (b) divides (a), pipe to foreach
+            } | % {
+                $x / $_ # perform division and roll that out
+            } 
+        } # increment total
     }
 }
 
