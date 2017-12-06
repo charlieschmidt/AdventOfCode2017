917eb2305ee1977f0fa56e612f49838ed36db664
Charlie Schmidt <charlie@charlieschmidt.me>
2017-12-05 21:48:45 -0500

    add comments for day 1

--- day1.ps1
+++ day1.ps1
@@ -12,5 +12,9 @@ process
         $offset = $in.length / 2
     }
 
-    0..$in.length |? {$in[$_] -eq $in[($_ + $offset) % $in.length]} | % {[string]$in[$_]} | measure -sum | select -expand sum
+    0..$in.length |? { # iterate the string indexes, pipe to where
+        $in[$_] -eq $in[($_ + $offset) % $in.length] # value at index is equal to value at index + offset (modulo length to wrap around)
+    } | % { # foreach from the where
+        [string]$in[$_] # output the value at that index (cast to string so the integer cast later isnt performed on a char)
+    } | measure -sum | select -expand sum # sum them up
 }
