c7257b2c99d3da7d190f627bf18b7789a07887c8
Charlie Schmidt <charlie.schmidt@equifax.com>
2017-12-07 09:51:22 -0500

    day7 parday7 parday7 parday7 parday7 parday7 parday7 part1 sorta-single pipeline

--- day7.ps1
+++ day7.ps1
@@ -6,10 +6,19 @@ param (
 )
 
 begin {
+    $nodes = @()
 }
 
 process {
+    $nodes += $in |? {
+        $in -match '^(?<Node>[a-z]+) \((?<Weight>\d+)\)(?: -> ){0,1}(?<ChildNodes>(?:(?:[a-z]+)(?:, ){0,1})+)*$'
+    } | % { 
+        [pscustomobject]$matches
+    } | select Node, Weight, @{n = "Children"; e= {$_.ChildNodes -split ", "}}
 }
 
-end {     
+end { 
+    $nodes |? {
+        $_.Node -notin ($nodes.Children)
+    } | select -expand node
 }
