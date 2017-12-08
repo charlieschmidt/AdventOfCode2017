9fdc205e632e1df980b0ef41f2c2129ea6fab925
Charlie Schmidt <charlie@charlieschmidt.me>
2017-12-08 00:37:12 -0500

    more comments in day7part2

--- day7.ps1
+++ day7.ps1
@@ -42,9 +42,15 @@ end {
         $queue = new-object system.collections.queue
 
         # this is a modification of a breadth-first algorithm, where we'll load a queue with the root
+        # loop til the queue is empty doing:
+        #   pop the queue
+        #   find childnodes of that element
+        #   write out the element on the pipeline
+        #   add them to the queue
+        # pipeline on the outside now has the elements in the order we want (leaves first, root last)
         $queue.Enqueue($root)
         
-        & { while ($queue.Count -ne 0) { # start inifinite pipeline, iterate til the queue is empty
+        & { while ($queue.Count -ne 0) { # start generator pipeline, iterate til the queue is empty
             $queue.Dequeue() # pop the first element
         } } | % { 
             # find all the actual child node objects, add that as a property to the element and PASS IT THROUGH THE PIPELINE
@@ -53,12 +59,13 @@ end {
             # add the childnodes to the queue
             $_.ChildNodes | ? {$_} | % { $queue.Enqueue($_) }
         } | Reverse | %{ # reverse the output from the ordering above - now the first elements in the pipeline are the leafs of the graph
+            # we can add subweight here at the same time we reference it on childnodes because we've ordered the list so that children always come before their parents
             $_ | add-member -notepropertyname Subweight -notepropertyvalue ([int]$_.weight + [int]($_.ChildNodes.Subweight | Measure -sum | select -expand Sum)) -passthru  #calculate the subweight and pass the object on
         } |? {
             ($_.ChildNodes.subweight | select -unique).count -gt 1 # find the object that has too many unique subweights [they should all be the same]
-        } | select -first 1 |% { # select the first (farthest into the graph) we find
+        } | select -first 1 |% { # select the first (farthest/highest leaf into the graph) we find
             $g = $_.ChildNodes | group subweight | sort-object count # find the two different subweights, first element is the broken one
-            # it should be its weight minus the difference in the subweights
+            # balanced weight = its weight minus the difference in the subweights
             $g[0].group[0].weight - ($g[0].group[0].subweight - $g[1].group[0].Subweight)
         }
     } 
